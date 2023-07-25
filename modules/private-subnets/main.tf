4c9952736407edde0268f0cf1efa66d463fd4b86
sowjiarha <128574497+sowjiarha@users.noreply.github.com>
2023-07-25 12:25:19 +0530

    Update main.tf

--- modules/private-subnets/main.tf
+++ modules/private-subnets/main.tf
@@ -1,27 +1,33 @@
 resource "aws_subnet" "pri-sub" {
   vpc_id = var.vpc_id
-  cidr_block = var.pri_cidr
+  count = length(var.pri_cidr)
+  cidr_block = var.pri_cidr[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
   tags = {
-    Name = "private-subnet-1"
+    Name = "private-${count.index}"
   }
 }
+data "aws_availability_zones" "available" {}
+
 resource "aws_route_table" "private-r" {
   vpc_id = var.vpc_id
   tags = {
-    Name = "private-routtable"
+    Name = "private-routetable"
   }
 }
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.pri-sub.id
+  count = length(var.pri_cidr)
+  subnet_id      = aws_subnet.pri-sub.*.id[count.index]
   route_table_id = aws_route_table.private-r.id
 }
 resource "aws_network_acl_association" "main" {
+  count = length(var.pri_cidr)
   network_acl_id = var.nacl_id
-  subnet_id      = aws_subnet.pri-sub.id
+  subnet_id      = aws_subnet.pri-sub.*.id[count.index]
 }
 resource "aws_route" "r" {
    route_table_id = aws_route_table.private-r.id
    destination_cidr_block = "0.0.0.0/0"
    nat_gateway_id = var.nat_gateway_id
 }
-
