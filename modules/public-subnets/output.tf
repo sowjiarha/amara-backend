b5e2c0489739184947e2b8a3a08dee31c2d70d00
sowjiarha <128574497+sowjiarha@users.noreply.github.com>
2023-07-25 12:32:26 +0530

    Update output.tf

--- modules/public-subnets/output.tf
+++ modules/public-subnets/output.tf
@@ -1,9 +1,16 @@
 output "nacl_id" {
-  value = aws_network_acl.main.id
+  value = aws_network_acl.nacl.id
 }
-output "subnet_id" {
-  value = aws_subnet.main.id 
+output "subnets" {
+  value = aws_subnet.pub-sub.*.id
 }
 output "cidr_blocks" {
-  value = [var.pub_cidr]
+  #value = "${element(var.pub_cidr,count.index)}"
+ # value = [for s in var.pub_cidr : s.cidr_block]
+  value = ["10.1.1.0/24", "10.1.2.0/24"]
+  #value = var.pub_cidr[count.index]
+}
+output "subnet_id" {
+ # value = aws_subnet.pub-sub.*.id[count.0]
+   value= "${element(aws_subnet.pub-sub.*.id, 0)}"
 }
